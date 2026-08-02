f7d79f2cf68ac7d55044b98de358745fda00eb61
Haneul Lee <leehaneul990623@gmail.com>
2026-08-02 11:51:22 +0900

    configure Kotlin Spring Boot project

---
+++ src/main/kotlin/com/le2sky/spr/SprApplication.kt
@@ -0,0 +1,11 @@
+package com.le2sky.spr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprApplication
+
+fun main(args: Array<String>) {
+    runApplication<SprApplication>(*args)
+}
